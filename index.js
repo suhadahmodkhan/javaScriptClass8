b6308f8728ccc0b0114af786019ba6a6420a3409
suhadahmodkhan <suhadahmodkhan04@gmail.com>
2024-07-31 01:37:54 -0700

    first commiut

---
+++ index.js
@@ -0,0 +1,29 @@
+// console.log(document.getElementById('heading-tag'))
+const headingId = document.getElementById('heading-tag')
+// const headingClass = document.getElementsByClassName('heading-class')
+// const headingTag = document.getElementsByTagName("h1")
+// const headingQuery = document.querySelector("h1")
+// const headingQueryAll = document.querySelectorAll("h1 p")
+// headingId.style.backgroundColor = "tomato"
+// headingId.style.padding = "100px"
+// headingId.style.fontSize = "32px"
+// headingId.style.fontWeight ="bolder"
+// headingId.style.color ="white"
+// headingQuery.classList.add("background-green")
+// headingQuery.classList.remove("background-green")
+// headingQuery.style.backgroundColor = "green"
+// const headingTag = document.getElementById('heading-tag');
+// console.log(headingTag.innerText)
+// headingTag.innerText = `this a new heading tag from index.js`
+// console.log(headingTag.innerText)
+// let a = 5 ;
+// a = 10
+// console.log(a)
+// headingQuery.classList.add("text-white")
+// headingQuery.classList.remove("bg-red-500")
+// headingQuery.classList.add(`bg-black`)
+// headingQuery.classList.add("p-20")
+console.log(headingQuery.classList)
+// headingClass[0].innerHTML = `changed by index`
+
+
---
+++ ul.js
@@ -0,0 +1,18 @@
+const id = document.getElementById("heading-tag")
+const p = document.createElement("p")
+p.innerText = `jack-fruit`
+id.appendChild(p)
+// id.style.backgroundColor = "tomato"
+// id.style.padding = "100px"
+// id.style.fontSize = "32px"
+// id.style.fontWeight ="bolder"
+// id.style.color ="white"
+// id.style.width = "fit-content"
+// id.style.borderRadius = "20px"
+// id.style.textAlign = "center"
+const ulList = document.getElementById("unorderList")
+// console.log(id.parentNode.parentNode.parentNode.parentNode)
+
+const li = document.createElement("li")
+li.innerText = `jack-fruit`
+ulList.appendChild(li)
